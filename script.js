780a37923a0e334c8125f683ece9e21a93544821
Marcelo Marinho <128069413+Marcelo-Marinho@users.noreply.github.com>
2024-09-27 16:58:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+
+Links:
+
+whatsapp: https://wa.me/55019994122340
+github:   https://github.com/Marcelo-Marinho
+linkedin: https://www.linkedin.com/in/marcelo-marinho/
+celular:
+email:    https://mail.google.com/mail/u/0/?fs=1&to=marcelohenrique0050@gmail&tf=cm&cc=marcelohenrique0050@gmail.com
+fiverr:
+itch.io:
+
+*/
+function link_open(url) {
+  window.open(url, '_blank').focus();
+}
